4ed32ad323a8c45e6acd364ffebd27e8ebc0d961
abugh <abu@gmail.com>
2021-03-15 01:11:38 +0530

    add user & user creation

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core.apps.CoreConfig'
+    'rest_framework.authtoken',
+    'core.apps.CoreConfig',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
